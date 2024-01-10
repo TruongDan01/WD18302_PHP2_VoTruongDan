e0b349aa3ca7bb2460c13a776190fca7dc54ec3d
TruongDan01 <danvtpc06576@fpt.edu.vn>
2024-01-10 11:21:59 +0700

    Create files - BaseModel in the App\\ Model - BaseControl folder in the App\\ Controller - Route folder in the App\\ Core folder After creation use autoload to call the above classes into the index.php file outside the root folder

---
+++ Lab2/Bai5/App/Controller/BaseController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Controller;
+
+class BaseController {
+    public function __construct() {
+        echo "Đây là BaseControl. </br>";
+    }
+}
---
+++ Lab2/Bai5/App/Model/BaseModel.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Model;
+
+class BaseModel {
+    public function __construct() {
+        echo "Đây là BaseModel </br>";
+    }
+}
