6b09fbb29d4d7f6a4847b0bf054c9862229fe4b4
TruongDan01 <danvtpc06576@fpt.edu.vn>
2024-01-10 11:23:29 +0700

    In index.php, use autoload to call classes into the file:

---
+++ Lab2/Bai5/index.php
@@ -0,0 +1,11 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use App\Model\BaseModel;
+use App\Controller\BaseController;
+use App\Core\Route;
+
+$model = new BaseModel();
+$control = new BaseController();
+$route = new Route();
