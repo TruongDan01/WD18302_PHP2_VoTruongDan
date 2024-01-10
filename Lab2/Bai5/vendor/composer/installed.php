f4cdccf097504195f0c36ec3e231e5779ef9329a
TruongDan01 <danvtpc06576@fpt.edu.vn>
2024-01-10 11:22:58 +0700

    Configure composer.json file

---
+++ Lab2/Bai5/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7de8b44ab3f89efd81f9a1bd2a4dd976df5e2468',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7de8b44ab3f89efd81f9a1bd2a4dd976df5e2468',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
